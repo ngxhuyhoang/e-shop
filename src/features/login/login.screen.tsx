63d50e4b11738b8662baf4c938c4eb8aabcff75b
Admin <vantaodz1@gmail.com>
2024-01-08 16:24:02 +0700

    doing

--- src/features/login/login.screen.tsx
+++ src/features/login/login.screen.tsx
@@ -1,23 +1,73 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Text,
+  View,
+  TextInput,
+  Keyboard,
+  Button,
+  Alert,
+} from 'react-native';
 
 const Login = () => {
   const { navigate } = useNavigation<any>();
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [login, setLogin] = useState('');
+
+  const validateEmail = (email: string) => {
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleLogin = () => {
+    if (!validateEmail(email)) {
+      Alert.alert('Thông báo', 'Email không hợp lệ');
+      return;
+    }
+
+    if (password.trim() === '') {
+      Alert.alert('Thông báo', 'Password không được để trống');
+      return;
+    }
+
+    if (login) {
+      navigate("Login", { userData: { email, password } });
+      Alert.alert('Thông báo', 'Đăng nhập thất bại!')
+    } else {
+      Alert.alert('Thông báo', 'Đăng nhập thành công!')
+    }
+
+    navigate('MainStack');
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <TouchableOpacity
-        onPress={() => {
-          navigate('MainStack');
-        }}>
-        <Text>Login</Text>
-      </TouchableOpacity>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'stretch', paddingHorizontal: 16 }}>
+      <KeyboardAvoidingView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View>
+            <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign:'center' }}>Đăng nhập</Text>
+            <TextInput
+              placeholder="Email"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+            <TextInput
+              placeholder="Password"
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+
+            />
+            <View>
+              <Button title="Đăng nhập" onPress={handleLogin}
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </View>
   );
 };
