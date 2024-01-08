5f71d6eb6d0c01a6c706a312600a70c64881dc17
Phạm Đình Thanh Bình <thanhbinhtk052@gmail.com>
2024-01-08 17:29:44 +0700

    Doing

--- src/features/list-order/list-order.screen.tsx
+++ src/features/list-order/list-order.screen.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { FlatList, Image, Text, View } from 'react-native';
+import { style } from 'twrnc';
 
 const data = [
-  { id: 1, image: "https://bobui.vn/cms/wp-content/uploads/2022/08/SAN-PHAM-T9-WEB_11-scaled.jpg", name: "Product1", price: 200, quantity: 1, totalAmount: 200 },
+  { id: 1, image: "https://bobui.vn/cms/wp-content/uploads/2022/08/SAN-PHAM-T9-WEB_11-scaled.jpg", name: "Product1", price: 200, quantity: 1, totalAmount: 100 },
   { id: 2, image: "https://bobui.vn/cms/wp-content/uploads/2022/08/SAN-PHAM-T9-WEB_11-scaled.jpg", name: "Product2", price: 100, quantity: 3, totalAmount: 300 },
   { id: 3, image: "https://bobui.vn/cms/wp-content/uploads/2022/08/SAN-PHAM-T9-WEB_11-scaled.jpg", name: "Product3", price: 200, quantity: 2, totalAmount: 400 },
   { id: 4, image: "https://bobui.vn/cms/wp-content/uploads/2022/08/SAN-PHAM-T9-WEB_11-scaled.jpg", name: "Product4", price: 50, quantity: 1, totalAmount: 50 }
@@ -18,33 +20,45 @@ interface ItemProps {
 }
 
 const Item = ({ id, image, name, price, quantity, totalAmount }: ItemProps) => (
-  <View>
-    <Text>{id}</Text>
-    <Image source= {{image}}></Image>
-    <Text>{name}</Text>
-    <Text>{price}</Text>
-    <Text>{quantity}</Text>
-    <Text>{totalAmount}</Text>
+  <View style={styles.container}>
+    <Image source={{ uri: image }} style={styles.image} />
+    <View>
+      <Text>{id}</Text>
+      <Text>{name}</Text>
+      <Text>Đơn giá: {price}</Text>
+      <Text>Số lượng: {quantity}</Text>
+      <Text>Tổng tiền sản phẩm: {totalAmount}</Text>
+    </View>
   </View>
 );
 
 const renderItem = ({ item }: any) => (
-  <Item id={item.id} image={item.image} name={item.name } price={item.price} quantity={item.quantity} totalAmount={item.totalAmount} />
+  <Item id={item.id} image={item.image} name={item.name} price={item.price} quantity={item.quantity} totalAmount={item.totalAmount} />
 );
 
 const ListOrder = () => {
   return (
     <View>
-      <Text>List Order</Text>
+      <Text>Danh sách đơn hàng</Text>
       {data && (
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
-    )}
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+        // keyExtractor={(item) => item.id}
+        />
+      )}
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'row'
+  },
+  image: {
+    width: 80,
+    height: 80
+  }
+})
 export default ListOrder;
