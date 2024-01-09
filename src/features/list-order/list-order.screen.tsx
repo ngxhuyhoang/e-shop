2873a714d7597e65d24ae85583cdf289d822eaba
Phạm Đình Thanh Bình <thanhbinhtk052@gmail.com>
2024-01-09 13:58:03 +0700

    on working shopping cart

--- src/features/list-order/list-order.screen.tsx
+++ src/features/list-order/list-order.screen.tsx
@@ -1,10 +1,10 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 import { FlatList, Image, Text, View } from 'react-native';
-import { style } from 'twrnc';
 
 const data = [
-  { id: 1, image: "https://bobui.vn/cms/wp-content/uploads/2022/08/SAN-PHAM-T9-WEB_11-scaled.jpg", name: "Product1", price: 200, quantity: 1, totalAmount: 100 },
+  { id: 1, image: "https://bobui.vn/cms/wp-content/uploads/2022/08/SAN-PHAM-T9-WEB_11-scaled.jpg", name: "Product1", price: 200, quantity: 1, totalAmount: 200 },
   { id: 2, image: "https://bobui.vn/cms/wp-content/uploads/2022/08/SAN-PHAM-T9-WEB_11-scaled.jpg", name: "Product2", price: 100, quantity: 3, totalAmount: 300 },
   { id: 3, image: "https://bobui.vn/cms/wp-content/uploads/2022/08/SAN-PHAM-T9-WEB_11-scaled.jpg", name: "Product3", price: 200, quantity: 2, totalAmount: 400 },
   { id: 4, image: "https://bobui.vn/cms/wp-content/uploads/2022/08/SAN-PHAM-T9-WEB_11-scaled.jpg", name: "Product4", price: 50, quantity: 1, totalAmount: 50 },
@@ -38,12 +38,24 @@ const Item = ({ id, image, name, price, quantity, totalAmount }: ItemProps) => (
 );
 
 
-const ListOrder = () => {
+const ListOrder = (props: any) => {
+  const navigation = useNavigation();
+
   const renderItem = ({ item }: any) => (
     <Item id={item.id} image={item.image} name={item.name} price={item.price} quantity={item.quantity} totalAmount={item.totalAmount} />
   );
+
+  const onPress = () => {
+    navigation.navigate('ShoppingCart');
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: '#DDDDDD' }}>
+      <Button
+        onPress={onPress}
+        title="Shopping Cart"
+      />
+
       <Text style={{ backgroundColor: 'white', paddingLeft: 30, paddingBottom: 30, paddingTop: 20, fontSize: 25 }}>Đơn hàng</Text>
       <FlatList
         data={data}
--- src/navigators/main.stack.tsx
+++ src/navigators/main.stack.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import AppTab from './app.tab';
+import ShoppingCart from '../features/shopping-cart/shopping-cart.screen';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ const MainStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="AppTab" component={AppTab} />
+      <Stack.Screen name="ShoppingCart" component={ShoppingCart} />
     </Stack.Navigator>
   );
 };
