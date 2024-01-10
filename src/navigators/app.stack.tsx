a0d837d0dcd87b94954fc0111877d4651d869f66
Admin <vantaodz1@gmail.com>
2024-01-10 17:21:59 +0700

    feat: update

--- src/navigators/app.stack.tsx
+++ src/navigators/app.stack.tsx
@@ -1,12 +1,31 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainStack from './main.stack';
 import AuthStack from './auth.stack';
+import { useNavigation } from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
 const AppStack = () => {
+  const { navigate } = useNavigation<any>();
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const token = await AsyncStorage.getItem('userToken');
+        console.log(token)
+
+        if (token) {
+          navigate('MainStack');
+        } else {
+          navigate('AuthStack');
+        }
+      } catch (error) {
+        console.error('Error checking login status:', error);
+      }
+    };
+    checkLoginStatus();
+  }, [navigate]); 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="AuthStack" component={AuthStack} />
