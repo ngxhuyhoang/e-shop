02ce4cd2d041270c4062e6e1986de113dfdaf383
Hung Quang Kieu <rptmasterk@gmail.com>
2024-01-15 15:33:25 +0700

    Change UI list product and detalproduct

--- src/features/list-product/list-detailProduct.screen.tsx
+++ src/features/list-product/list-detailProduct.screen.tsx
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import NavBar from '../nav-bar/nav-bar';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const ProductDetailPage = ({ route }) => {
   const { product } = route.params;
@@ -23,15 +24,34 @@ const ProductDetailPage = ({ route }) => {
       <ScrollView style={{ flex: 1 }}>
         <Image
           source={{ uri: product.image }}
-          style={{ width: '100%', height: 300 }}
+          style={{
+            width: '100%',
+            height: 300,
+            resizeMode: 'contain',
+            backgroundColor: 'white',
+          }}
         />
         <View
           style={{
             backgroundColor: 'white',
             marginTop: 12,
           }}>
-          <Text style={styles.productName}>{product.title}</Text>
+          <Text style={styles.productName} numberOfLines={2}>
+            {product.title}
+          </Text>
           <Text style={styles.productPrice}>{product.price} $</Text>
+          <View style={{ height: 40, flexDirection: 'row' }}>
+            <Image
+              source={require('../../../pics/rating2.png')}
+              style={{
+                width: 60,
+                height: 42,
+                tintColor: 'orange',
+                marginHorizontal: 8,
+              }}
+            />
+            <Text style={{ alignItems: 'center', marginTop: 12 }}>5</Text>
+          </View>
         </View>
         <View style={{ backgroundColor: 'white', marginTop: 12 }}>
           <View
@@ -67,6 +87,54 @@ const ProductDetailPage = ({ route }) => {
           </View>
         </View>
       </ScrollView>
+      <View style={styles.tabBar}>
+        <TouchableOpacity
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#00b3b3',
+            height: 50,
+          }}>
+          <Ionicons
+            name="chatbubble-ellipses-outline"
+            size={24}
+            color="white"
+          />
+        </TouchableOpacity>
+        <View>
+          <Image
+            source={require('../../../pics/line.png')}
+            style={{ height: 50, width: 1 }}
+            tintColor={'#808080'}
+          />
+        </View>
+        <TouchableOpacity
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#00b3b3',
+            height: 50,
+          }}>
+          <Image
+            source={require('../../../pics/add-to-cart.png')}
+            style={{ width: 24, height: 24 }}
+            tintColor="white"
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            flex: 2,
+            height: 50,
+            backgroundColor: '#ff5c33',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text style={{ color: 'white', fontSize: 16 }}>Mua với voucher</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -92,6 +160,12 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginVertical: 4,
     marginHorizontal: 4,
+  },
+  tabBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 50,
   },
 });
 
